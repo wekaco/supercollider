77d36eb2ca990a8aaba1ba4dfdcd51ea61ebf45c
Brian Heim <brianlheim@gmail.com>
2018-09-09 13:38:45 -0500

    no auto updatega

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -4,12 +4,12 @@ export BREW_NO_AUTO_UPDATE=1
 export BREW_NO_ANALYTICS=1
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
-brew install ccache
+BREW_NO_AUTO_UPDATE=1 brew install ccache
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 
-brew install libsndfile || true
-brew install portaudio || true
-brew install qt5 || true
+BREW_NO_AUTO_UPDATE=1 brew install libsndfile || true
+BREW_NO_AUTO_UPDATE=1 brew install portaudio || true
+BREW_NO_AUTO_UPDATE=1 brew install qt5 || true
 brew link qt5 --force
 
 # To get less noise in xcode output
