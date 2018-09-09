831986e12408881f5ca8064107466cda993536d2
Brian Heim <brianlheim@gmail.com>
2018-09-09 13:43:01 -0500

    force bottle?

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-export BREW_NO_AUTO_UPDATE=1
 export BREW_NO_ANALYTICS=1
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
-BREW_NO_AUTO_UPDATE=1 brew install ccache
+brew install ccache --force-bottle
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 
-BREW_NO_AUTO_UPDATE=1 brew install libsndfile || true
-BREW_NO_AUTO_UPDATE=1 brew install portaudio || true
-BREW_NO_AUTO_UPDATE=1 brew install qt5 || true
+brew install libsndfile --force-bottle || true
+brew install portaudio --force-bottle || true
+brew install qt5 --force-bottle || true
 brew link qt5 --force
 
 # To get less noise in xcode output
